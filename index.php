6fdbacb6a141440ffbb91cf81e18192e9cc47c0d
anthoox <kray.a4@gmail.com>
2024-03-20 12:06:57 +0100

    Configuración autoload.php y añadiendo validación en index.php de lista-simple

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <!-- Cabeceras -->
+<?php require_once 'autoload.php'; ?>
 <?php require_once 'C:/wamp64/www/lista-simple/views/layout/head.php'; ?>
+
+<?php
+if (isset($_GET['controller'])) {
+    $nombre_controlador = $_GET['controller'] . 'Controller';
+} else {
+    echo "La pagina que buscas no existe";
+    // exit() detiene la ejecución.
+    exit();
+}
+
+
+if (class_exists($nombre_controlador)) {
+    $controlador = new $nombre_controlador();
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+        $action = $_GET['action'];
+        $controlador->$action();
+    } else {
+        echo 'La pagiana que buscas no EXISTE';
+    }
+} else {
+    echo 'La pagiana que buscas no existeeee';
+}
+?>
 
 <!-- Contenido -->
 
