34ca1d66681d28be50e6174f2953c428d49c514d
anthoox <kray.a4@gmail.com>
2024-07-19 10:58:34 +0200

    añadiendo las cookies en helpers creando una clase con un método estatico

--- views/users/home.php
+++ views/users/home.php
@@ -3,31 +3,12 @@
 <?php require_once base_host . 'controllers/UsersController.php'; ?>
 <?php require_once base_host . 'models/users.php'; ?>
 <?php require_once base_host . 'views/layout/head.php'; ?>
+<?php require_once base_host . 'helpers/cookies.php'; ?>
+
 
 <?php
-
-if (isset($_COOKIE['user_email']) && isset($_COOKIE['user_password'])) {
-
-
-    // Identificar usuario
-    $user = new User();
-    $user->setEmail($_COOKIE['user_email']);
-    $user->setPassword($_COOKIE['user_password']);
-
-    // Consulta a la base de datos
-    $identity = $user->login();
-
-    // Iniciar la sesión
-    if ($identity && is_object($identity)) {
-        $_SESSION['identity'] = $identity;
-        if ($identity->rol == 1) {
-            $_SESSION['admin'] = true;
-        } elseif ($identity->rol == 2) {
-            $_SESSION['user'] = true;
-        }
-    }
-    require_once base_host . 'views/login/login.php';
-}
+// Comprobamos si existen las cookies en el navegador para redigir
+Cookies::cookiesHome();
 
 ?>
 
