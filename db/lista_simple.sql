e872a32b0e17aa421532f2abaaa2e79513c89e08
anthoox <kray.a4@gmail.com>
2024-07-29 00:41:28 +0200

    añadiendo los datos de la nueva versión de la bd

--- db/lista_simple.sql
+++ db/lista_simple.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 07-05-2024 a las 11:25:59
--- Versión del servidor: 8.2.0
--- Versión de PHP: 8.2.13
+-- Tiempo de generación: 28-07-2024 a las 22:26:10
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.1.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,14 +35,12 @@ CREATE TABLE IF NOT EXISTS `items` (
   `name` varchar(255) NOT NULL,
   `price` float(100,2) DEFAULT '0.00',
   `numer` int DEFAULT '1',
-  -- `notification_date` datetime DEFAULT NULL,
-  -- `notes` text,
   `completed` int DEFAULT '0',
   `creation_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_items_users` (`user_id`),
   KEY `fk_items_lists` (`list_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -57,13 +55,12 @@ CREATE TABLE IF NOT EXISTS `lists` (
   `name` varchar(255) NOT NULL,
   `creation_date` datetime DEFAULT NULL,
   `modification_date` date DEFAULT NULL,
-  -- `notification` datetime DEFAULT NULL,
-  -- `description` text, 
   `paper_bin` int NOT NULL DEFAULT '0',
   `completed` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_lists_users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- --------------------------------------------------------
 
@@ -82,17 +79,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `up_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `registration_date`, `rol`, `image`) VALUES
-(1, 'Admin', 'admin@admin.com', '$2y$04$Ndtn9LXqojgpR7LuIVhYXeAI8N8msRnsToDPFYSQXgCnr2q6nKAtC', '2024-04-09', 1, NULL),
-(9, 'Test', 'test@test.com', '$2y$04$vjEKOpKlQ5.3/lFAark3V.Q1j4HyJA21GKZN.eP9M2xtG78w9R7Q6', '2024-04-15', 2, NULL),
-(17, 'Prueba', 'prueba@prueba.com', '$2y$04$I6zVQEuZdid4c5lCld7wP.1haSiZL4MkodUigjfrzU/OhYvGDzVE2', '2024-05-05', 2, NULL);
+
+(1, 'Prueba', 'prueba@prueba.com', '$2y$04$I6zVQEuZdid4c5lCld7wP.1haSiZL4MkodUigjfrzU/OhYvGDzVE2', '2024-05-05', 2, NULL);
 
 --
 -- Restricciones para tablas volcadas
